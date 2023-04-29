202ef9e02551ac1b2d4e8d736818d67384ef4d49
Alan Vitório <alanvitorio1985@gmail.com>
2023-04-28 23:39:57 -0300

    add Headers e Nav

--- src/UserContext.js
+++ src/UserContext.js
@@ -11,17 +11,13 @@ export const UserStorage = ({ children }) => {
   const [error, setError] = React.useState(null);
   const navigate = useNavigate();
 
-  const userLogout = React.useCallback(
-    async function() {
-      setData(null);
-      setError(null);
-      setLoading(false);
-      setLogin(false);
-      window.localStorage.removeItem('token');
-      navigate('/login');
-    },
-    [navigate],
-  );
+  const userLogout = React.useCallback(async function() {
+    setData(null);
+    setError(null);
+    setLoading(false);
+    setLogin(false);
+    window.localStorage.removeItem('token');
+  }, []);
 
   async function getUser(token) {
     const { url, options } = USER_GET(token);
@@ -70,6 +66,8 @@ export const UserStorage = ({ children }) => {
           setLogin(true);
           setLoading(false);
         }
+      } else {
+        setLogin(false);
       }
     }
     autoLogin();
