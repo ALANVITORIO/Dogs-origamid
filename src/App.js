98ac46b5011a71c896a5e7fa9e9bdaffd3d3e58d
Alan Vitório <alanvitorio1985@gmail.com>
2023-05-10 03:23:38 -0300

    rota foto feed add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Home from './Components/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
 
 function App() {
   return (
@@ -26,8 +27,8 @@ function App() {
                 </ProtectedRoute>
               }
             />
+            <Route path="foto/:id" element={<Photo />} />
           </Routes>
-
           <Footer />
         </UserStorage>
       </BrowserRouter>
